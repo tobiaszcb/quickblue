73aa70fa088215370bc234f22e5f86b84084d6e8
Tobiasz <tobiaszbarczynski@gmail.com>
2022-03-27 01:03:35 +0100

    Change app icon path resolution to use abspath of __file__

--- src/globals.py
+++ src/globals.py
@@ -1,4 +1,4 @@
 from os.path import abspath
 
 APP_INDICATOR_ID = "quickblue-indicator"
-APP_ICON = abspath("../airpods_icon.png")
+APP_ICON = abspath(__file__).replace("/src/globals.py", "/airpods_icon.png", 1)
